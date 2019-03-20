0081afb9f6e016832cc2c240ba7d00368183ed11
Huy Phung <huy.phung@uni-konstanz.de>
2019-03-20 20:18:00 +0100

    done varm

--- scripts/config.m
+++ scripts/config.m
@@ -6,15 +6,18 @@ function cfg = config()
     %% "verify" :
     %%     - ARIMA and VAR simply load the optimal params founded before.
     %%     - LSTM load the pretrained weights
-    cfg.execMode = "rerun";
+    cfg.execMode = "train";
     %cfg.execMode = "verify";
     
     %% Dataset mode :
     %% "full"   : 245445 records, extremely computationally heavy
     %% "sample" : 14880 records, easier to compute
-    cfg.dataset.mode = "full"; 
-    %cfg.dataset.mode = "sample;
+    %cfg.dataset.mode = "full"; 
+    cfg.dataset.mode = "sample";
     cfg.dataset.trainSetRatio = 0.8; % ratio=0.8 -> train:val:test = 8:1:1
+    
+    %% Maximum lags (how many timesteps we look back), used by all models.
+    cfg.maxLags = 3;
     
     %% ARIMA saved optimal parameters
     cfg.arima.params = {...
@@ -23,19 +26,18 @@ function cfg = config()
         20 0 6;... %% Params for Low
         20 0 6;... %% Params for Close
         };
+    cfg.arima.savedModels = "";
     
     %% VAR saved optimal parameters
     cfg.var.P = 53;
+    cfg.varm.savedModel = "";
     
     %% LSTM uni variate saved weight
-    cfg.lstm.uni.openModelFile = "";
+    cfg.lstm.uni.savedModels = "";
  
     %% LSTM multivariate saved weight
     cfg.lstm.multi.openModelFile = "";
-    
-    %% Rerun parameter, max lags
-    cfg.lstm.maxlags = 50;
-    
+        
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     csvPathFull = "EURUSD_15m_BID_01.01.2010-31.12.2016.csv";
     csvPathSample = "EURUSD_15m_BID_sample.csv";
--- scripts/varmMain.m
+++ scripts/varmMain.m
@@ -2,41 +2,44 @@
 cfg = config();
 
 [eurusd, featureNames] = eurusdDataset(cfg.dataset.csvPath, "");
-[eurusdTrain, ~, eurusdTest] = eurusdPartition(...
+[YTrain, ~, YTest] = eurusdPartition(...
     eurusd, cfg.dataset.trainSetRatio);
+YTrain = YTrain(:, 1:4);
+YTest = YTest(:, 1:4);
 
-if cfg.execMode == "rerun"
-    [estMdl, YPred, aic, rmse, mape] = appVarmRerun(YTrain, YTest);
+if cfg.execMode == "train"
+    [estMdl, YPred, aic, rmse, mape] = appVarmTrain(...
+        YTrain, YTest, featureNames, cfg);
 else
     p = cfg.var.P;
     [estMdl, YPred, aic, rmse, mape] = appVarmVerify(YTrain, YTest, P);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [varMdl, YPred, aic, rmse, mape] = appVarRerun(YTrain, YTest)
-    [varMdl, aic] = varmSearchParams(YTrain);
-    [YPred, rmse, mape] = varmPredict(YTest, 1);
-    visualizeResult(varMdl, YPred, YTest, rmse, mape);
-    
+function [varmMdl, YPred, aic, rmse, mape] = appVarmTrain(...
+        YTrain, YTest, featureNames, cfg)
+    [varmMdl, aic] = varmSearchParams(YTrain, cfg.maxLags);
+    [YPred, rmse, mape] = varmPredict(varmMdl, YTest, 1);
+    save(modelFilename(), 'varmMdl');
+    visualizeResult(varmMdl, YPred, YTest, rmse, mape, featureNames);
 end
 
-function [YPred, aic, rmse, mape] = appVerify(YTrain, YTest, P)
-    mdl = varm(dimInput, P);
-    estMdl = estimate(mdl, YTrain);
-    results = summarize(estMdl);
-    aic = results.AIC;
-    
-    
+function filename = modelFilename()
+    filename = strcat('varma_ohlc_', datestr(now,'yyyymmddTHHMMSS'));
 end
 
+function [YPred, aic, rmse, mape] = appVarmVerify(YTest, cfg)
+    load(cfg.varm.savedModel, 'varmMdl');
+    varmMdlSummary = summarize(varmMdl);
+    aic = varmMdlSummary.aic;
+    [YPred, rmse, mape] = varmPredict(varmMdl, YTest, 1);
+end
 
-function [varMdl, aic] = varmSearchParams(YTrain)
+function [varmMdl, aic] = varmSearchParams(YTrain, maxLags)
     dimInput = size(YTrain, 2);
-    
     minAic = Inf;
     minMdl = varm(dimInput, 1);
-    
-    for lag = 1:60
+    for lag = 1:maxLags
         mdl = varm(dimInput, lag);
         estMdl = estimate(mdl, YTrain);
         results = summarize(estMdl);
@@ -46,22 +49,46 @@ function [varMdl, aic] = varmSearchParams(YTrain)
             minMdl = estMdl;
         end
     end
-    
-    varMdl = minMdl;
+    varmMdl = minMdl;
 end
 
-function visualizeResult(estMdl, YTest, YPred, mape, rmse)
+function [YPred, mape, rmse] = varmPredict(estMdl, YTest, numResponse)
+    %% Predict num_response values ahead from model and Y
+    YTest = YTest.';
+    YPred = zeros(size(YTest));
+    lags = estMdl.P;
+    YPred(:, 1:lags) = YTest(1);
+    i = 1;
+    while i <= (size(YTest, 2) - lags)
+        observations = YTest(:, i:(i+lags-1));
+        [tmp, ~] = forecast(estMdl, numResponse, observations.');
+        YPred(:,(i+lags):(i+lags+numResponse-1)) = tmp;
+        i = i + numResponse;
+    end
+    [rmse, mape] = computePredError(YTest, YPred);
+    YPred = YPred.';
+end
+
+
+function [errRmse, errMape] = computePredError(YTest, YPred)
+    errRmse = rmse(YTest, YPred);
+    errMape = mape(YTest, YPred);
+end
+
+function visualizeResult(estMdl, YTest, YPred, mapeErr, rmseErr, featureNames)
     %% Plot the predicted value, observed value, and error
     modelDesc = strcat("VAR(", num2str(estMdl.P), ")");
-    figureTag = strcat(modelDesc + " on EURUSD BID, ", featureName, " price");
-    figure('Name', figureTag);
-    candle(YTest(:, 500:1000).', 'b')
-    hold on
-    candle(YPred(:, 500:1000).', 'r')
-    hold off
-    ylabel("EURUSD(BUY)")
-    title("RMSE=" + rmse + " MAPE=" + mape);
-end
-
-function [YPred, mape, rmse] = varmPredict(estMdl, Ytest, numResponse)
+    for i = 1:4 
+        uniRmse = rmse(YTest(:, i), YPred(:, i));
+        uniMape = mape(YTest(:, i), YPred(:, i));
+        figureTag = strcat(modelDesc, ", EURUSD BID, ", featureNames(i));
+        figure('Name', figureTag)
+        plot(YTest(:, i), 'b')
+        hold on
+        plot(YPred(:, i), 'r')
+        hold off
+        legend(["Observed" "Predicted"])
+        ylabel("EURUSD")
+        title(featureNames(i) + ": RMSE=" + uniRmse + " MAPE=" + uniMape);
+    end
 end
