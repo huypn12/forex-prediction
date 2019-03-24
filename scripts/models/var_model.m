111ef72f20b5b3361544fd6b0087b369c3a5d526
huy.phung <huy.phung@uni-konstanz.de>
2019-03-24 20:18:22 +0100

    remove junk files

--- scripts/models/var_model.m
+++
@@ -1,9 +0,0 @@
-function model = varma_fx(yTrain, maxLag)
-
-dimInput = size(yTrain, 2);
-varmModel = varm(dimInput, maxLag);
-estModel = estimate(varmModel, yTrain);
-
-model = estModel;
-
-end
--- scripts/tests/test_dataset.m
+++
@@ -1,6 +0,0 @@
-cfg = config();
-
-eurusd = fx_dataset(cfg.dataset_sample, "");
-
-std_eurusd = fx_standardize(eurusd);
-trainset = fx_trainset(std_eurusd, 100);
